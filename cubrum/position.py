a915e6c94f12da0ad45ff9f2a0a8901caea43301
Francis O'Neill <francis.d.oneill@erdc.dren.mil>
2025-08-07 11:36:27 -0400

    Implemented ColumnPosition.setOrientation()

--- cubrum/position.py
+++ cubrum/position.py
@@ -59,6 +59,9 @@ class PointPosition:
         except AssertionError as e:
             raise InvalidPositionError(e)
         
+    def copy(self) -> "PointPosition":
+        return PointPosition(mapLocation=self.mapLocation, map=self.map, orientation=self.orientation, distanceToDestination=self.distanceToDestination)
+    
     def getPositionType(self) -> str:
         if type(self.mapLocation)==tuple:
             return "edge"
@@ -78,11 +81,28 @@ class PointPosition:
         try:
             if self.getPositionType()=="edge":
                 assert orientation in self.mapLocation, "node '{}' is not an endpoint of edge '{}'".format(orientation, self.mapLocation)
+                if orientation != self.orientation:
+                    new_distance_to_destination = self.getDescription()['distance'] - self.distanceToDestination
+                else:
+                    new_distance_to_destination = self.distanceToDestination
             else:
-                assert orientation in self.map.neighbors(self.mapLocation), "orientation '{}' is not a neighbor of mapLocation '{}'".format(orientation, self.mapLocation)
+                assert (orientation is None) or (orientation in self.map.neighbors(self.mapLocation)), "orientation '{}' is not a neighbor of mapLocation '{}'".format(orientation, self.mapLocation)
+                new_distance_to_destination = self.distanceToDestination
         except AssertionError as e:
             raise InvalidActionError(e)
         self.orientation = orientation
+        self.distanceToDestination = new_distance_to_destination
+
+    def reverseCourse(self) -> None:
+        self.validate()
+        if self.getPositionType()=="node":
+            raise InvalidActionError("Cannot reverse course while on a node")
+        if not self.orientation:
+            raise InvalidActionError("Cannot reverse course when orientation is not set")
+        new_orientation = self.mapLocation[0] if self.mapLocation[0]!=self.orientation else self.mapLocation[1]
+        new_distance_to_destination = self.getDescription()['distance'] - self.distanceToDestination
+        self.setOrientation(new_orientation)
+        self.distanceToDestination = new_distance_to_destination
 
     def move(self, distance:float, toward:str=None) -> DecisionPoint:
         """Update position based on travel distance
@@ -143,8 +163,6 @@ class PointPosition:
             shortest_path = self.map.getShortestPath(self.mapLocation, other.mapLocation)
             path_length = self.map.getPathLength(shortest_path)
             return path_length
-        elif set(self.mapLocation)==set(other.mapLocation): # same-edge case
-            pass
         elif (self.getPositionType()=="edge") and (other.getPositionType()=="node"): # self edge, other node
             distance_choices = []
             for node_name in self.mapLocation: # iterate over edges
@@ -160,6 +178,12 @@ class PointPosition:
             # reverse case already handled, so just switch self and other
             return other.getDistance(self)
         else: # both edges
+            if set(self.mapLocation)==set(other.mapLocation): # same-edge case
+                if self.orientation==other.orientation: # same edge, pointed the same way
+                    return abs(other.distanceToDestination-self.distanceToDestination) 
+                else: # same edge, pointed opposite ways
+                    others_distance_to_self_destination = (other.getDescription()["distance"]-other.distanceToDestination)
+                    return abs(others_distance_to_self_destination-self.distanceToDestination)
             distance_choices = []
             for node_name in self.mapLocation: # iterate over edges
                 node_position = PointPosition(node_name, map=self.map)
@@ -180,9 +204,134 @@ class ColumnPosition:
     Attributes:
         vanPosition: PointPosition of vanguard
         rearPosition: PointPosition of rearguard
+        columnLength: full extent of column on the march
 
     Methods:
+        validate() -> None 
         reverseCourse() -> None
+        getCurrentLength() -> float
+        setDestination() -> None
 
     """
-    pass
+    def __init__(self, vanPosition:PointPosition, rearPosition:PointPosition=None, columnLength:float=None, waypoints:list=None):
+        self.vanPosition = vanPosition
+        self.rearPosition = (rearPosition or vanPosition.copy())
+        self.columnLength = (columnLength or self.GetCurrentLength())
+        self.waypoints = (waypoints or [])
+        self.waypoints = list(self.waypoints)
+
+    def validate(self):
+        self.vanPosition.validate()
+        self.rearPosition.validate()
+        try:
+            assert self.getCurrentLength() <= self.columnLength, "column length {} greater than maximum length {}".format(self.getCurrentLength(), self.columnLength)
+            if len(self.waypoints) > 0:
+                # if there are waypoints 
+                # validate waypoint adjacency
+                if len(self.waypoints) > 1:
+                    for i in range(1, len(self.waypoints)):
+                        test_edge = (self.waypoints[i-1], self.waypoints[i])
+                        try:
+                            self.vanPosition.map.edges[test_edge]
+                        except:
+                            raise InvalidPositionError("waypoints '{}' and '{}' are not adjacent".format(self.waypoints[i-1], self.waypoints[i]))
+                # check rearPosition
+                if self.rearPosition.getPositionType()=="node":
+                    assert self.rearPosition.mapLocation!=self.waypoints[-1], "last waypoint '{}' is the same as rearPosition '{}'".format(self.waypoints[-1], self.rearPosition.mapLocation)
+                    assert self.rearPosition.mapLocation in self.rearPosition.map.neighbors(self.waypoints[-1]), "rearPosition '{}' not adjacent to last waypoint '{}'".format(self.rearPosition.mapLocation, self.waypoints[-1])
+                else:
+                    assert self.waypoints[-1] in self.rearPosition.mapLocation, "last waypoint '{}' not an endpoint of rearPosition '{}'".format(self.waypoints[-1], self.rearPosition.mapLocation)
+                    if self.rearPosition.orientation:
+                        assert self.rearPosition.orientation==self.waypoints[-1], "rearPosition is oriented away from last waypoint '{}'".format(self.waypoints[-1])
+                # check vanPosition
+                if self.vanPosition.getPositionType()=="node":
+                    assert self.waypoints[0]!=self.vanPosition.mapLocation, "first waypoint '{}' is the same as vanPosition '{}'".format(self.waypoints[0], self.vanPosition.mapLocation)
+                    assert self.vanPosition.mapLocation in self.vanPosition.map.neighbors(self.waypoints[0]), "vanPosition '{}' not adjacent to first waypoint '{}'".format(self.vanPosition.mapLocation, self.waypoints[0])
+                else:
+                    assert self.waypoints[0] in self.vanPosition.mapLocation, "first waypoint '{}' not an endpoint of vanPosition '{}'".format(self.waypoints[0], self.vanPosition.mapLocation)
+                    if self.vanPosition.orientation:
+                        pass
+            else:
+                # if van and rear are on the same edge or node
+                if self.vanPosition.getPositionType()==self.rearPosition.getPositionType():
+                    assert (self.vanPosition.mapLocation==self.rearPosition.mapLocation) or (set(self.vanPosition.mapLocation)==set(self.rearPosition.mapLocation)), "no waypoints, but vanPosition '{}' and rearPosition '{}' are not the same".format(self.vanPosition.mapLocation, self.rearPosition.mapLocation)
+                    if self.vanPosition.getPositionType()=="edge": # both edges
+                        if self.vanPosition.orientation==self.rearPosition.orientation: # oriented the same way
+                            assert self.vanPosition.distanceToDestination<=self.rearPosition.distanceToDestination, "rearPosition ahead of vanPosition"
+                        else: # oriented opposite ways, only valid if "shrinking"
+                            assert self.vanPosition.distanceToDestination>(self.rearPosition.getDescription()['distance']-self.rearPosition.distanceToDestination), "rearPosition oriented away from vanPosition"
+                elif self.vanPosition.getPositionType()=="node": # van node, rear edge
+                    assert self.vanPosition.mapLocation in self.rearPosition.mapLocation, "no waypoints, but vanPosition node '{}' is not part of rearPosition edge '{}'".format(self.vanPosition.mapLocation, self.rearPosition.mapLocation)
+                    assert self.rearPosition.orientation==self.vanPosition.mapLocation, "rearPosition not oriented toward vanPosition"
+                elif self.rearPosition.getPositionType()=="node": # van edge, rear node
+                    assert self.rearPosition.mapLocation in self.vanPosition.mapLocation, "no waypoints, but rearPosition node '{}' is not part of vanPosition edge '{}'".format(self.rearPosition.mapLocation, self.vanPosition.mapLocation)
+                    assert self.rearPosition.orientation==(self.vanPosition.mapLocation[0] if self.vanPosition.mapLocation[1]==self.rearPosition.mapLocation else self.vanPosition.mapLocation[1]), "rearPosition not oriented toward vanPosition"
+                    assert self.vanPosition.orientation!=self.rearPosition.mapLocation, "vanPosition cannot shrink towards static rearPosition"
+        except AssertionError as e:
+            raise InvalidPositionError(e)
+
+    def getCurrentLength(self) -> float:
+        """Return current extent of column"""
+        return self.vanPosition.getDistance(self.rearPosition)
+    
+    def reverseCourse(self) -> None:
+        """Swap van and rear"""
+        self.waypoints = [self.waypoints[i] for i in range(len(self.waypoints)-1,-1,-1)] # reverse list
+        tempVan = self.rearPosition.copy()
+        if tempVan.getPositionType()=="edge":
+            try:
+                tempVan.reverseCourse()
+            except InvalidActionError:
+                pass
+        else:
+            tempVan.setOrientation(None)
+        tempRear = self.vanPosition.copy()
+        if tempRear.getPositionType()=="edge":
+            try:
+                tempRear.reverseCourse()
+            except InvalidActionError:
+                pass
+        else:
+            if len(self.waypoints)>0:
+                tempRear.setOrientation(self.waypoints[-1])
+            else:
+                tempRear.setOrientation(tempVan.orientation)
+        self.vanPosition = tempVan
+        self.rearPosition = tempRear
+
+    def setOrientation(self, new_orientation) -> None:
+        if new_orientation==self.vanPosition.orientation:
+            # new orientation is current orientation
+            pass 
+        elif new_orientation==self.vanPosition.mapLocation:
+            # new orientation is current vanPosition location
+            pass
+        elif new_orientation==self.rearPosition.mapLocation:
+            # new orientation is current rearPosition location
+            self.reverseCourse()
+        elif new_orientation in self.waypoints:
+            # new orientation is an existing waypoint
+            self.vanPosition.setOrientation(self.waypoints[0])
+            self.rearPosition.setOrientation(self.waypoints[-1])
+        elif (self.vanPosition.getPositionType()=="node") and (new_orientation in self.vanPosition.map.neighbors(self.vanPosition.mapLocation)):
+            # new orientation is neighbor of vanPosition node
+            if (self.rearPosition.getPositionType()=="edge") and (new_orientation in self.rearPosition.mapLocation):
+                self.reverseCourse() 
+            else:
+                self.vanPosition.setOrientation(new_orientation)
+        elif (self.rearPosition.getPositionType()=="node") and (new_orientation in self.vanPosition.map.neighbors(self.rearPosition.mapLocation)):
+            # new orientation is neighbor of rearPosition node
+            self.reverseCourse()
+            self.vanPosition.setOrientation(new_orientation)
+        elif (self.vanPosition.getPositionType()=="edge") and (new_orientation in self.vanPosition.mapLocation):
+            # new orientation is reverse of current vanPosition orientation
+            # we already know it's not the current orientation, so reverse course 
+            if len(self.waypoints) > 0:
+                # if there are waypoints, turn inward to shrink
+                self.vanPosition.reverseCourse()
+            else:
+                # otherwise just reverse column
+                self.reverseCourse()
+        else:
+            raise InvalidActionError("Cannot set orientation of column with vanPosition '{}', rearPosition '{}', and waypoints '{}' to '{}'".format(self.vanPosition.mapLocation, self.rearPosition.mapLocation, self.waypoints, new_orientation))
+        self.validate()
--- tests/test_position.py
+++ tests/test_position.py
@@ -9,9 +9,11 @@ import numpy as np
 
 import cubrum.position
 import cubrum.map 
+from cubrum.exceptions import InvalidPositionError
 
 COPPERCOAST_NODES_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "cubrum", "mapdata", "coppercoast_strongholds.json")
 COPPERCOAST_ROADS_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "cubrum", "mapdata", "coppercoast_roads.json")
+
 
 class TestPointPosition(unittest.TestCase):
     def setUp(self):
@@ -37,7 +39,7 @@ class TestPointPosition(unittest.TestCase):
         distance_ep = position_edge.getDistance(position_point)
         self.assertEqual(distance_expected, distance_ep)
 
-    def testGetDistanceBothEdges(self):
+    def testGetDistanceDifferentEdges(self):
         position_edge1 = cubrum.position.PointPosition(("Oughan Keep", "Joukra"), orientation="Oughan Keep", distanceToDestination=1, map=self.roads)
         position_edge2 = cubrum.position.PointPosition(("Oughan Keep", "Smara"), orientation="Oughan Keep", distanceToDestination=1, map=self.roads)
         distance_expected = 2
@@ -45,6 +47,182 @@ class TestPointPosition(unittest.TestCase):
         self.assertEqual(distance_expected, distance_12)
         distance_21 = position_edge2.getDistance(position_edge1)
         self.assertEqual(distance_expected, distance_21)
+
+    def testGetDistanceSameEdge(self):
+        position_edge1 = cubrum.position.PointPosition(("Oughan Keep", "Smara"), orientation="Oughan Keep", distanceToDestination=2, map=self.roads)
+        position_edge2 = cubrum.position.PointPosition(("Oughan Keep", "Smara"), orientation="Oughan Keep", distanceToDestination=1, map=self.roads)
+        distance_expected = 1
+        distance_12 = position_edge1.getDistance(position_edge2)
+        self.assertEqual(distance_expected, distance_12)
+        distance_21 = position_edge2.getDistance(position_edge1)
+        self.assertEqual(distance_expected, distance_21)
+
+
+class TestColumnPosition(unittest.TestCase):
+    def setUp(self):
+        self.roads = cubrum.map.Map()
+        self.roads.addNodesFromFile(COPPERCOAST_NODES_PATH)
+        self.roads.addEdgesFromFile(COPPERCOAST_ROADS_PATH)
+
+    def testStaticNode(self):
+        van_position = cubrum.position.PointPosition("Orbost", map=self.roads)
+        rear_position = cubrum.position.PointPosition("Orbost", map=self.roads)
+        column_position = cubrum.position.ColumnPosition(vanPosition=van_position, rearPosition=rear_position, columnLength=0.2)
+        try:
+            column_position.validate()
+            self.assertTrue(True)
+        except InvalidPositionError as e:
+            raise e
+        
+    def testTravelingNode(self):
+        van_position = cubrum.position.PointPosition("Orbost", orientation="Ulgis", map=self.roads)
+        rear_position = cubrum.position.PointPosition("Orbost", orientation="Ulgis", map=self.roads)
+        column_position = cubrum.position.ColumnPosition(vanPosition=van_position, rearPosition=rear_position, columnLength=0.2)
+        try:
+            column_position.validate()
+            self.assertTrue(True)
+        except InvalidPositionError as e:
+            raise e
+
+    def testTravelingEdge(self):
+        van_position = cubrum.position.PointPosition(("Orbost", "Ulgis"), orientation="Ulgis", distanceToDestination=0.5, map=self.roads)
+        rear_position = cubrum.position.PointPosition(("Ulgis", "Orbost"), orientation="Ulgis", distanceToDestination=0.6, map=self.roads)
+        column_position = cubrum.position.ColumnPosition(vanPosition=van_position, rearPosition=rear_position, columnLength=0.2)
+        try:
+            column_position.validate()
+            self.assertTrue(True)
+        except InvalidPositionError as e:
+            raise e
+        
+    def testShrinkingNoWaypoints(self):
+        van_position = cubrum.position.PointPosition(("Lugana", "Kuragang"), orientation="Lugana", distanceToDestination=6, map=self.roads)
+        rear_position = cubrum.position.PointPosition(("Lugana", "Kuragang"), orientation="Kuragang", distanceToDestination=6.1, map=self.roads)
+        column_position = cubrum.position.ColumnPosition(vanPosition=van_position, rearPosition=rear_position, columnLength=0.2)
+        try:
+            column_position.validate()
+            self.assertTrue(True)
+        except InvalidPositionError as e:
+            raise e
+        
+    def testTravelingOneWayPoint(self):
+        van_position = cubrum.position.PointPosition(("Brick Bridge", "Kuragang"), orientation="Brick Bridge", distanceToDestination=1.9, map=self.roads)
+        rear_position = cubrum.position.PointPosition(("Lugana", "Kuragang"), orientation="Kuragang", distanceToDestination=0.05, map=self.roads)
+        column_position = cubrum.position.ColumnPosition(vanPosition=van_position, rearPosition=rear_position, waypoints=["Kuragang"], columnLength=0.2)
+        try:
+            column_position.validate()
+            self.assertTrue(True)
+        except InvalidPositionError as e:
+            raise e
+        
+    def testShrinkingOneWayPoint(self):
+        van_position = cubrum.position.PointPosition(("Brick Bridge", "Kuragang"), orientation="Kuragang", distanceToDestination=0.1, map=self.roads)
+        rear_position = cubrum.position.PointPosition(("Lugana", "Kuragang"), orientation="Kuragang", distanceToDestination=0.05, map=self.roads)
+        column_position = cubrum.position.ColumnPosition(vanPosition=van_position, rearPosition=rear_position, waypoints=["Kuragang"], columnLength=0.2)
+        try:
+            column_position.validate()
+            self.assertTrue(True)
+        except InvalidPositionError as e:
+            raise e
+        
+    def testTravelingTwoWayPoints(self):
+        van_position = cubrum.position.PointPosition(("Neruga's Gate", "Nkaa"), orientation="Neruga's Gate", distanceToDestination=7, map=self.roads)
+        rear_position = cubrum.position.PointPosition(("Purann", "Sultan's Rock"), orientation="Sultan's Rock", distanceToDestination=1, map=self.roads)
+        column_position = cubrum.position.ColumnPosition(vanPosition=van_position, rearPosition=rear_position, waypoints=["Nkaa", "Sultan's Rock"], columnLength=5)
+        try:
+            column_position.validate()
+            self.assertTrue(True)
+        except InvalidPositionError as e:
+            raise e
+
+    def testReverseCourseNoWaypoints(self):
+        van_position = cubrum.position.PointPosition(("Orbost", "Ulgis"), orientation="Ulgis", distanceToDestination=0.5, map=self.roads)
+        rear_position = cubrum.position.PointPosition(("Ulgis", "Orbost"), orientation="Ulgis", distanceToDestination=0.6, map=self.roads)
+        column_position = cubrum.position.ColumnPosition(vanPosition=van_position, rearPosition=rear_position, columnLength=0.2)
+        try:
+            column_position.validate()
+            self.assertTrue(True)
+        except InvalidPositionError as e:
+            raise InvalidPositionError("Failed before course reversal: {}".format(e))
+        column_position.reverseCourse()
+        try:
+            column_position.validate()
+            self.assertTrue(True)
+        except InvalidPositionError as e:
+            raise e
+        
+    def testReverseCourseTwoWaypoints(self):
+        van_position = cubrum.position.PointPosition(("Neruga's Gate", "Nkaa"), orientation="Neruga's Gate", distanceToDestination=7, map=self.roads)
+        rear_position = cubrum.position.PointPosition(("Purann", "Sultan's Rock"), orientation="Sultan's Rock", distanceToDestination=1, map=self.roads)
+        column_position = cubrum.position.ColumnPosition(vanPosition=van_position, rearPosition=rear_position, waypoints=["Nkaa", "Sultan's Rock"], columnLength=5)
+        try:
+            column_position.validate()
+            self.assertTrue(True)
+        except InvalidPositionError as e:
+            raise InvalidPositionError("Failed before course reversal: {}".format(e))
+        column_position.reverseCourse()
+        try:
+            column_position.validate()
+            self.assertTrue(True)
+        except InvalidPositionError as e:
+            raise e
+        
+    def testSetOrientationOneEdge(self):
+        van_position = cubrum.position.PointPosition(("Orbost", "Ulgis"), orientation="Ulgis", distanceToDestination=0.5, map=self.roads)
+        rear_position = cubrum.position.PointPosition(("Ulgis", "Orbost"), orientation="Ulgis", distanceToDestination=0.6, map=self.roads)
+        column_position = cubrum.position.ColumnPosition(vanPosition=van_position, rearPosition=rear_position, columnLength=0.2)
+        try:
+            column_position.validate()
+            self.assertTrue(True)
+        except InvalidPositionError as e:
+            raise InvalidPositionError("Failed before course change: {}".format(e))
+        column_length_before_course_change = column_position.getCurrentLength()
+        column_position.setOrientation("Orbost")
+        try:
+            column_position.validate()
+            self.assertEqual(column_position.vanPosition.orientation, "Orbost")
+            np.testing.assert_allclose(column_length_before_course_change, column_position.getCurrentLength())
+        except InvalidPositionError as e:
+            raise e
+        
+    def testSetOrientationNodeEdge(self):
+        van_position = cubrum.position.PointPosition("Ulgis", map=self.roads)
+        rear_position = cubrum.position.PointPosition(("Ulgis", "Orbost"), orientation="Ulgis", distanceToDestination=0.1, map=self.roads)
+        column_position = cubrum.position.ColumnPosition(vanPosition=van_position, rearPosition=rear_position, columnLength=0.2)
+        try:
+            column_position.validate()
+            self.assertTrue(True)
+        except InvalidPositionError as e:
+            raise InvalidPositionError("Failed before course change: {}".format(e))
+        column_length_before_course_change = column_position.getCurrentLength()
+        column_position.setOrientation("Orbost")
+        try:
+            column_position.validate()
+            self.assertEqual(column_position.vanPosition.orientation, "Orbost")
+            self.assertEqual(column_position.rearPosition.mapLocation, "Ulgis")
+            np.testing.assert_allclose(column_length_before_course_change, column_position.getCurrentLength())
+        except InvalidPositionError as e:
+            raise e
+        
+    def testSetOrientationWayPoints(self):
+        van_position = cubrum.position.PointPosition(("Ulgis", "Orbost"), orientation="Orbost", distanceToDestination=3, map=self.roads)
+        rear_position = cubrum.position.PointPosition(("Ulgis", "Nabiac"), orientation="Ulgis", distanceToDestination=1, map=self.roads)
+        column_position = cubrum.position.ColumnPosition(vanPosition=van_position, rearPosition=rear_position, waypoints=["Ulgis"], columnLength=4)
+        try:
+            column_position.validate()
+            self.assertTrue(True)
+        except InvalidPositionError as e:
+            raise InvalidPositionError("Failed before course change: {}".format(e))
+        column_length_before_course_change = column_position.getCurrentLength()
+        column_position.setOrientation("Ulgis")
+        try:
+            column_position.validate()
+            self.assertEqual(set(column_position.vanPosition.mapLocation), set(("Ulgis","Orbost")))
+            self.assertEqual(set(column_position.rearPosition.mapLocation), set(("Ulgis","Nabiac")))
+            self.assertEqual(column_position.vanPosition.orientation, "Ulgis")
+            self.assertEqual(column_position.rearPosition.orientation, "Ulgis")
+            np.testing.assert_allclose(column_length_before_course_change, column_position.getCurrentLength())
+        except InvalidPositionError as e:
+            raise e
 
 def main():
     log.info("Creating map")
