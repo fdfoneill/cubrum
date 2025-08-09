74ae6e4c25f858498c888c8a23a2c54b4f9f8692
F. Dan O'Neill <fdfoneill@gmail.com>
2025-08-09 19:49:46 -0400

    Begin docstring for Warrior

--- cubrum/warrior.py
+++ cubrum/warrior.py
@@ -3,4 +3,18 @@ logging.basicConfig(level=os.environ.get("LOGLEVEL","INFO"))
 log = logging.getLogger(__name__)
 
 class Warrior:
+    """A single human being; a fighter
+    
+    ***
+    
+    Attributes:
+        name:str
+        age:int
+        title:str
+        rank:int
+        culture:cubrum.culture.Culture
+        
+    Methods:
+        
+    """
     pass
