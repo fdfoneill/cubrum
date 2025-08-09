8563161010f6b08fcaf500c4be8a2c77193e84db
F. Dan O'Neill <fdfoneill@gmail.com>
2025-08-09 19:50:40 -0400

    Convenience function to create toy Army

--- cubrum/__init__.py
+++ cubrum/__init__.py
@@ -2,21 +2,23 @@ import logging, os
 logging.basicConfig(level=os.environ.get("LOGLEVEL","INFO"))
 log = logging.getLogger(__name__)
 
-from . import position, map
+from . import map, army, formation, commander
 
 COPPERCOAST_NODES_PATH = os.path.join(os.path.dirname(__file__), "mapdata", "coppercoast_strongholds.json")
 COPPERCOAST_ROADS_PATH = os.path.join(os.path.dirname(__file__), "mapdata", "coppercoast_roads.json")
 
-def initializeColumn(stronghold_name:str, column_length:float=0.3) -> position.ColumnPosition:
+def initializeArmy(starting_stronghold:str=None, formations:list=None) -> army.Army:
     assert os.path.exists(COPPERCOAST_NODES_PATH)
     assert os.path.exists(COPPERCOAST_ROADS_PATH)
     coppercoast = map.Map()
     coppercoast.addNodesFromFile(COPPERCOAST_NODES_PATH)
     coppercoast.addEdgesFromFile(COPPERCOAST_ROADS_PATH)
-    assert stronghold_name in coppercoast.nodes
-    van_position = position.PointPosition(stronghold_name, map=coppercoast)
-    rear_position = position.PointPosition(stronghold_name, map=coppercoast)
-    column_position = position.ColumnPosition(vanPosition=van_position, rearPosition=rear_position, columnLength=column_length)
-    return column_position
+    starting_stronghold=starting_stronghold or "Yinnagul"
+    assert starting_stronghold in coppercoast.nodes
+    cavalry = formation.Formation(name="1st Yinnagul Light Cavalry", warriorCount=200, wagonCount=0, cavalry=True)
+    recon_force = army.Army(name="Yinnagul Reconnaisance Force", formations=(formations or [cavalry]), commander=commander.Commander(), supply=cavalry.getSupplyCapacity(), startingStronghold=starting_stronghold,map=coppercoast)
+    return recon_force
+    
+    
 
 
