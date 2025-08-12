ebf2fe68bc9dcce93b2b48e653be64a0c8e159bb
Francis O'Neill <francis.d.oneill@erdc.dren.mil>
2025-08-12 09:31:20 -0400

    Added pedigree

--- cubrum/commander.py
+++ cubrum/commander.py
@@ -19,10 +19,14 @@ class Commander(Warrior):
     Methods:
         
     """
-    def __init__(self, name:str, age:int, title:str, culture=None, commanderTraits:list=None):
+    def __init__(self, name:str, age:int, title:str, pedigree:str=None, culture=None, commanderTraits:list=None):
         super().__init__(name, age, culture)
         self.title=title
+        self.pedigree=pedigree
         self.commanderTraits = (commanderTraits or [])
 
     def __repr__(self):
-        return "{} {}".format(self.title, self.name)
+        repr_string = "{} {}".format(self.title, self.name)
+        if self.pedigree:
+            repr_string += " {}".format(self.pedigree)
+        return repr_string
