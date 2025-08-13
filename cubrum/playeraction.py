833a5934aacca2a2d9a803d3a08e359047d26a62
Francis O'Neill <francis.d.oneill@erdc.dren.mil>
2025-08-13 11:29:40 -0400

    Working implementation of Proceed

--- cubrum/playeraction.py
+++ cubrum/playeraction.py
@@ -18,16 +18,20 @@ class PlayerAction:
         actionName: name of chosen action 
         startDate: date action was taken
     """
-    def __init__(self, playerID:int, actionName:str):
-        self.id = playerID
+    def __init__(self, playerID:int, hours:int, actionName:str):
+        self.playerID = playerID
         self.actionName = str(actionName)
+        self.hours=int(hours)
 
     def apply(self, state:GameState):
         pass
 
     def isValid(self, state:GameState) -> bool:
-        if self.id not in state.getPlayers():
+        if self.playerID not in state.getPlayers(): # check player exists
             return False 
+        if self.hours>0: # only active player can take actions with a nonzero time
+            if not state.clock.getActivePlayer()==self.playerID:
+                return False
         return True
 
 
@@ -96,7 +100,36 @@ class PillageStronghold(PlayerAction):
 
 
 class Proceed(PlayerAction):
-    pass
+    def __init__(self, playerID:int, hours:int=1):
+        super().__init__(playerID=playerID, hours=hours, actionName="Proceed")
+
+    def __repr__(self):
+        return "player={} proceed in present course of action for {} hours".format(self.playerID, self.hours)
+
+    def isValid(self, state):
+        if not super().isValid(state):
+            return False 
+        army_geometries = state.getArmyGeometries()[state.playerToArmy[self.playerID]]
+        if len(army_geometries['touching']) > 0:
+            return False 
+        if len(army_geometries['intersecting']) > 0:
+            return False
+        return True
+
+        
+    def apply(self, state) -> list:
+        decision_points = []
+        army = state.armies[state.playerToArmy[self.playerID]]
+        if army.position.getMotion()!="holding":
+            res_march = army.march(hours=self.hours)
+            if res_march:
+                res_march.updateContext(playerID=self.playerID)
+                decision_points.append(res_march)
+        res_clock = state.clock.incrementPlayerTime(self.playerID, self.hours)
+        if res_clock:
+            res_clock.updateContext(playerID=self.playerID)
+            decision_points.append(res_clock)
+        
 
 
 class ReconstructSiegeEngines(PlayerAction):
